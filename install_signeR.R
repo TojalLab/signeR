7ffe084f6c467d44d11f3b2450de34daaffcb552
rvalieris <rvalieris@users.noreply.github.com>
2016-06-15 18:14:06 -0300

    updated install script and manuals

--- install_signeR.R
+++ install_signeR.R
@@ -1,18 +1,30 @@
+#
+# this helper script will install signeR and its dependencies
+#
 
-# load bioconductor
-source("http://bioconductor.org/biocLite.R")
-biocLite(ask=FALSE)
+local({
+	target_R <- "3.2.2"
+	if(getRversion() < target_R) {
+		warning(paste0("R version detected (",getRversion(),") is below the recommended (",target_R,"), consider upgrading R."))
+	}
 
-if(!require("devtools")) { biocLite("devtools", ask=FALSE) } # attempt to install devtools
+	# dependencies
+	deps <- c("R.oo", "VariantAnnotation", "RcppArmadillo", "nloptr", "NMF", "class", "tensorA", "devtools")
 
-if(require("devtools")) {
-	# devtools loaded
-	devtools::install_github("rvalieris/signeR")
-} else {
-	# something wrong with devtools, attempt manual installation
-	biocLite(c("BiocGenerics","Biostrings","BSgenome","class",
-		"GenomicRanges","nloptr","NMF","plot3D","R.methodsS3",
-		"R.oo","tensorA","VariantAnnotation","Rcpp","RcppArmadillo"))
-	install.packages("https://github.com/rvalieris/signeR/releases/download/0.5.0/signeR_0.5.0.tar.gz")
-}
+	# load bioconductor
+	source("http://bioconductor.org/biocLite.R")
 
+	# attempt to install missing packages
+	for(p in deps[!(deps %in% installed.packages())]) {
+		biocLite(p, suppressUpdates=TRUE)
+		if(length(find.package(p, quiet=TRUE)) == 0) { # installation failed
+			stop(paste0("failed to install package: ",p,"."))
+		}
+	}
+
+	# install the latest signeR package
+	devtools::install_url("https://github.com/rvalieris/signeR/releases/download/v0.5.0/signeR_0.5.0.tar.gz")
+
+	# update bioconductor
+	biocLite()
+})
