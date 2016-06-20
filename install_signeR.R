e99bd78ded3189b3ab3bfcabaf3654acfae3068c
rvalieris <rvalieris@users.noreply.github.com>
2016-06-20 12:25:48 -0300

    updated docs, vignette, install script

--- install_signeR.R
+++ install_signeR.R
@@ -39,7 +39,7 @@ local({
 	}
 
 	# install the latest signeR package
-	devtools::install_url("https://github.com/rvalieris/signeR/releases/download/v0.99.1/signeR_0.99.1.tar.gz")
+	devtools::install_url("https://github.com/rvalieris/signeR/releases/download/v0.99.3/signeR_0.99.3.tar.gz")
 
 	# update bioconductor
 	biocLite()
